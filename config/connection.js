af48d540ad4f21d3710ba12c2069ba41ea1a5a61
elaine01 <ecumming.g@gmail.com>
2018-04-09 21:57:11 -0700

    Maria

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 let connection;
 
-if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
+if (process.env.JAWSDB_MARIA_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
 } else {
 	connection = mysql.createConnection({
 		host: "localhost",
